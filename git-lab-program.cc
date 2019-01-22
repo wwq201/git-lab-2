c2dac781b0911913c3d064907d79e3039b2277ec
WeiqiangWang <ww669616@ohio.edu>
2019-01-22 11:38:14 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author:Weiqiang
+ *        Date:1/22/2019
+ * Description: lab1 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
